56e4980d925ee9ed0aa8ac61a9e002460f746fcc
Bobby Rong <rjl931189261@126.com>
2021-03-17 19:38:05 +0800

    add the hash table structure

---
+++ src/hash_table.h
@@ -0,0 +1,15 @@
+// hash_table.h
+
+// A key-value pair.
+typedef struct {
+    char* key;
+    char* value;
+} ht_item;
+
+// The hash table.
+typedef struct {
+    int size;
+    int count;
+    ht_item** items;
+} ht_hash_table;
+
