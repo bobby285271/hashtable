be09a385d75b26a646e0c9e3357e050784cf5d27
Bobby Rong <rjl931189261@126.com>
2021-03-17 19:39:16 +0800

    code format

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,15 +1,16 @@
 // hash_table.h
 
 // A key-value pair.
-typedef struct {
-    char* key;
-    char* value;
+typedef struct
+{
+    char *key;
+    char *value;
 } ht_item;
 
 // The hash table.
-typedef struct {
+typedef struct
+{
     int size;
     int count;
-    ht_item** items;
+    ht_item **items;
 } ht_hash_table;
-
