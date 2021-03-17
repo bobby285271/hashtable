91832df30244c341b5c42643454feea39a2f4479
Bobby Rong <rjl931189261@126.com>
2021-03-17 19:45:21 +0800

    add initialisation functions

---
+++ src/hash_table.c
@@ -0,0 +1,26 @@
+// hash_table.c
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+// initialisation function for ht_item
+// static: only ever be called by code internal to the hash table
+static ht_item *ht_new_item(const char *k, const char *v)
+{
+    ht_item *i = malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->value = strdup(v);
+    return i;
+}
+
+// initialisation function for ht_hash_table
+ht_hash_table *ht_new()
+{
+    ht_hash_table *ht = malloc(sizeof(ht_hash_table));
+    ht->size = 52; // TODO: resizing
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item *));
+    return ht;
+}
