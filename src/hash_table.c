881aaa8bddc79e82a12c11ca9889ca9c7a99ded2
Bobby Rong <rjl931189261@126.com>
2021-03-18 09:10:18 +0800

    add basic APIs

--- src/hash_table.c
+++ src/hash_table.c
@@ -9,6 +9,8 @@
 
 #include "hash_table.h"
 
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
 // initialisation function for ht_item
 // static: only ever be called by code internal to the hash table
 static ht_item *ht_new_item(const char *k, const char *v)
@@ -73,6 +75,66 @@ static int ht_get_hash(const char *s, const int num_buckets, const int attempt)
     const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
     const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+void ht_insert(ht_hash_table *ht, const char *key, const char *value)
+{
+    ht_item *item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item *cur_item = ht->items[index];
+    int i = 1;
+    // iterate through indexes until we find an empty bucket
+    while (cur_item != NULL)
+    {
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
+// checks whether the item's key matches the key we're searching for
+// returns the item's value
+char *ht_search(ht_hash_table *ht, const char *key)
+{
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item *item = ht->items[index];
+    int i = 1;
+    while (item != NULL)
+    {
+        if (strcmp(item->key, key) == 0)
+        {
+            return item->value;
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    return NULL;
+}
+
+// mark an item as deleted by replacing it with a pointer to a global sentinel item
+void ht_delete(ht_hash_table *ht, const char *key)
+{
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item *item = ht->items[index];
+    int i = 1;
+    while (item != NULL)
+    {
+        if (item != &HT_DELETED_ITEM)
+        {
+            if (strcmp(item->key, key) == 0)
+            {
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    ht->count--;
 }
 
 int main()
